c0917467b657622adf5ee578fe0a957c784b7dbd
ayushkulkarni2819 <67851273+ayushkulkarni2819@users.noreply.github.com>
2020-07-05 11:19:56 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+
+var moving,fixed;
+
+function setup() {
+  createCanvas(800,400);
+  fixed=createSprite(400, 200, 50, 50);
+ moving=createSprite(500, 200, 40, 40);
+ fixed.shapeColor="green";
+ moving.shapeColor="green";
+}
+function draw() {
+  background("blue");  
+  moving.x=mouseX;
+  moving.y=mouseY;
+ 
+  if (moving.x-fixed.x<moving.width/2+fixed.width/2 &&
+    fixed.x-moving.x<moving.width/2+fixed.width/2 &&
+    moving.y-fixed.y<moving.height/2+fixed.height/2 &&
+    fixed.y-moving.y<moving.height/2+fixed.height/2) {
+    fixed.shapeColor="red";
+    moving.shapeColor="red";
+  } else  {
+    fixed.shapeColor="green";
+    moving.shapeColor="green";
+  }
+
+  drawSprites();
+}
